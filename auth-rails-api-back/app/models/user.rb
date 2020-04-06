38c0a7d8c6bd45152c031fa72c99f5f9a44bcb80
Tsunamins <reillyamr@gmail.com>
2020-04-06 11:35:50 -0400

    add relationships to secret, add name to migration

--- auth-rails-api-back/app/models/user.rb
+++ auth-rails-api-back/app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many: secrets
   validates :email, :name, presence: true
   validates :email, uniqueness: true
+  #why is a password validation ok to not be here, has_secure_password checks for password prescence
 end
