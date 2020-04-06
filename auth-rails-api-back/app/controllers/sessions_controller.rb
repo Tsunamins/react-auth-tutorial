571a79c8dc4ae2ae2605f5b5f13931476509360f
Tsunamins <reillyamr@gmail.com>
2020-04-06 16:29:41 -0400

    fix credentials, now responds to correct creds

--- auth-rails-api-back/app/controllers/sessions_controller.rb
+++ auth-rails-api-back/app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
     def create
-        #changeing these to params[:user][:email], is not part of fixing initial 500 server error, may not be needed
-        @user = User.find_by(email: params[:email])
+        #changeing these to params[:user][:email], is not part of fixing initial 500 server error
+        #these changes below, [:user][:email] and [:user][:password]
+        #ensure credentials are passed in, without these correct credentials throw the error "invalid credentials"
+        @user = User.find_by(email: params[:user][:email])
 
 
         #to fix 500 status error had to add @user.authenticate, before was @authenticate
-        if @user && @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:user][:password])
             #success
             render json: @user
         else
