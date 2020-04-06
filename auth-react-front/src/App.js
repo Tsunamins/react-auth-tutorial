7082e8b7c906773e7c3966571e69e5b5d8ea20c7
Tsunamins <reillyamr@gmail.com>
2020-04-06 16:16:23 -0400

    add e. handler funcs & make  controlled components

--- auth-react-front/src/App.js
+++ auth-react-front/src/App.js
@@ -8,15 +8,59 @@ class App extends React.Component {
     super()
     this.state = {
       currentUser: null, 
+      loginForm: {
+        email: "", 
+        password: ""
+      }
 
     }
+  }
+
+  handleLoginFormChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      loginForm: {
+        ...this.state.loginForm,
+        [name]: value
+
+      }
+
+    })
+  }
+
+  handleLoginFormSubmit = event => {
+    event.preventDefault()
+    const userInfo = this.state.loginForm
+    const headers = {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: userInfo
+      })
+    }
+    fetch("http://localhost:3000/login", headers)
+      .then(resp => resp.json())
+      .then(console.log)
+      .catch(console.log)
+    
+     
 
   }
+
   render() { return (
     <div className="App">
       Welcome User (using user name)(will need state in this context, bc not using redux yet)(login form needed, built through components)
       Done through Login.js
-      <Login />
+      <Login 
+        handleLoginFormChange={this.handleLoginFormChange}
+        
+        handleLoginFormSubmit={this.handleLoginFormSubmit}
+        // adding this below makes these controlled components and are passed into Login components params
+        email={this.state.loginForm.email}
+        password={this.state.loginForm.password}
+         />
     </div>
     );
   }
--- auth-react-front/src/components/Login.js
+++ auth-react-front/src/components/Login.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Login = ()=> {
+// adding email and password to the end of params makes controlled component, args are then passed in to input fields below
+const Login = ({handleLoginFormChange, handleLoginFormSubmit, email, password})=> {
    return(
        <div className="login">
-           <form>
+           <form onSubmit={handleLoginFormSubmit}>
                <br/>
-               <input type="text" name="email" placeholder="email"></input>
+               <input type="text" name="email" placeholder="email" onChange={handleLoginFormChange} value={email}></input>
                <br/>
-               <input type="text" name="password" placeholder="password"></input>
+               <input type="password" name="password" placeholder="password" onChange={handleLoginFormChange} value={password}></input>
                <br/>
                <input type="submit" value="login"></input>
            </form>
