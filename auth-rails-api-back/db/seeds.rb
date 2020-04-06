0ea95f26ca3af6fc1d30198c8b9521d64e22ba27
Tsunamins <reillyamr@gmail.com>
2020-04-06 13:06:37 -0400

    migrate and seed database

--- auth-rails-api-back/db/seeds.rb
+++ auth-rails-api-back/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chloe = User.create(name: "Chloe", email: "chloe@chloe.com", password: "password")
+graycie = User.create(name: "Graycie", email: "graycie@graycie.com", password: "password")
+april = User.create(name: "April", email: "april@april.com", password: "password")
+
+chloe.secrets.create(content: "I think I'm a dog!")
+graycie.secrets.create(content: "I am mischievious on purpose")
+april.secrets.create(content: "I like chewing on my fur")
+graycie.secrets.create(content: "I dream about my scratching post")
+april.secrets.create(content: "I want to learn to play video games")
