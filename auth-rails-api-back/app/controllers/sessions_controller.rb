9a34bab8c45e4643be1475ceb54dd196f6c09d4d
Tsunamins <reillyamr@gmail.com>
2020-04-06 16:26:51 -0400

    fix error, @user.authen, not permitg valid creds

--- auth-rails-api-back/app/controllers/sessions_controller.rb
+++ auth-rails-api-back/app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
     def create
+        #changeing these to params[:user][:email], is not part of fixing initial 500 server error, may not be needed
         @user = User.find_by(email: params[:email])
 
-        if @user && @authenticate(params[:password])
+
+        #to fix 500 status error had to add @user.authenticate, before was @authenticate
+        if @user && @user.authenticate(params[:password])
             #success
             render json: @user
         else
