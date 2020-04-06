316402f8389f5c09f7f529713589b23dbec75ab4
Tsunamins <reillyamr@gmail.com>
2020-04-06 13:39:35 -0400

    add login component, setup to get user info

--- auth-react-front/src/App.js
+++ auth-react-front/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 import './App.css';
+import Login from "./components/Login"
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      currentUser: null, 
+
+    }
+
+  }
+  render() { return (
     <div className="App">
-      Auth Lecture 
+      Welcome User (using user name)(will need state in this context, bc not using redux yet)(login form needed, built through components)
+      Done through Login.js
+      <Login />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
