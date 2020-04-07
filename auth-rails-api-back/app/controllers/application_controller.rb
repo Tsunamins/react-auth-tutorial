7e6bbceffd9a4830cae24d23fb279c26b224e605
Tsunamins <reillyamr@gmail.com>
2020-04-07 10:44:13 -0400

    add a mock system of login for demonstration only

--- auth-rails-api-back/app/controllers/application_controller.rb
+++ auth-rails-api-back/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+
+    def current_user
+        #for example purposes and temporary
+        User.first
+    end
+
+    def logged_in?
+        #for example purposes and temporary
+        true
+
+    end
+
 end
