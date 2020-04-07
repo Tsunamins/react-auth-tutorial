3cf46231266740b09590fb662b43864e7e342b7a
Tsunamins <reillyamr@gmail.com>
2020-04-07 10:57:25 -0400

    fix not a func error, note explanation

--- auth-react-front/src/components/Secrets.js
+++ auth-react-front/src/components/Secrets.js
@@ -3,12 +3,14 @@ import React from 'react';
 // currently an error is generated, secrets.map is not a function
 // has to do with, for react, not deconstructing props first
 //next commit will have solution
+//2 options: 1 = to change secrets before first arrow to props, then change secrets.map to props.secrets.map
+//2= change secrets before arrow to {(secrets)}, and leave secrets.map the same
 
-const Secrets = secrets => {
+const Secrets = ( {secrets} ) => {
     const secretsJSX = secrets.map(s => <p>{s.content}</p>)
     return(
         <div className="Secrets">
-            return {secretsJSX}
+             {secretsJSX}
          
          </div>
     )
