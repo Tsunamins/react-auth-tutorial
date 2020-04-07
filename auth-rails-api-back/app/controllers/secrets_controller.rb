dbc7bd037faf32213a8021ce27e332282c6adc11
Tsunamins <reillyamr@gmail.com>
2020-04-07 11:24:49 -0400

    fix error, rmv before_action and duplicate logic

--- auth-rails-api-back/app/controllers/secrets_controller.rb
+++ auth-rails-api-back/app/controllers/secrets_controller.rb
@@ -1,11 +1,19 @@
 class SecretsController < ApplicationController
-  before_action :set_secret, only: [:show, :update, :destroy]
+  #before_action :set_secret, only: [:show, :update, :destroy]
 
   # GET /secrets
   def index
-    @secrets = Secret.all
+    if logged_in?
+      @secrets = current_user.secrets
 
-    render json: @secrets
+      render json: @secrets, status: :ok
+
+    else
+      render json: {
+        error: "not logged in", status: :unauthorized
+      }
+
+    end
   end
 
   # GET /secrets/1
